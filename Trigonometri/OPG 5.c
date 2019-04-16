d5ccf2ed6a683af0d3c71cee71d6c620fbaee6bc
Mario0703 <42077299+Mario0703@users.noreply.github.com>
2019-04-16 14:05:09 +0200

    Update OPG 5.c

--- Trigonometri/OPG 5.c
+++ Trigonometri/OPG 5.c
@@ -17,15 +17,15 @@ int main(){
     printf("Indtast den modstaende kate, skriv 0 hvis ukendt\n");
     scanf("%f", &modstaende_katete);
 
-    if (hypotenusen == 0)//hvis hypotenusen skal programmet lave denne udregning
+    if (hypotenusen == 0)//hvis hypotenusen er =0 skal programmet lave denne udregning
     {
 
-    vinkel_A = atan(modstaende_katete/hosliggende_katet);//tangens
-    vinkel_A = radiantodegree(vinkel_A);//brug af funktion
+    vinkel_A = atan(modstaende_katete/hosliggende_katet);//tangens^-1 
+    vinkel_A = radiantodegree(vinkel_A);//brug af funktion der regner fra radians til grader
 
-    hypotenusen=sqrt(modstaende_katete*modstaende_katete+hosliggende_katet*hosliggende_katet);//pytagoras læresætning
+    hypotenusen=sqrt(modstaende_katete*modstaende_katete+hosliggende_katet*hosliggende_katet);//pytagoras læresætning isoleret til c=\sqrt(modstaende_katete^2+hosliggende_katet^2)
 
-    vinkel_B=180-vinkel_A-vinkel_C;
+    vinkel_B=180-vinkel_A-vinkel_C;//Finder vinkel B
 
     printf("\nVinkel A: %f\nVinkel B: %f\nVinkel C: %d\nSide   c: %f \n",vinkel_A,vinkel_B,vinkel_C, hypotenusen);
 
@@ -37,16 +37,16 @@ int main(){
 
     }
 
-    else if (hosliggende_katet==0)
+    else if (hosliggende_katet==0)//Hvis hosliggende katete er = 0 skal programmet lave denne udregning
 
     {
 
-    vinkel_A = asin(modstaende_katete/hypotenusen);//sinus
-    vinkel_A = radiantodegree(vinkel_A);//brug af funktion
+    vinkel_A = asin(modstaende_katete/hypotenusen);//sinus^-1
+    vinkel_A = radiantodegree(vinkel_A);//brug af funktion der regner fra radians til grader
 
-    hosliggende_katet = sqrt(hypotenusen*hypotenusen - modstaende_katete*modstaende_katete);//pytagoras læresætning
+    hosliggende_katet = sqrt(hypotenusen*hypotenusen - modstaende_katete*modstaende_katete);//pytagoras læresætning isoleret til b=sqrt(hypotenusen^2 - modstaende_katete^2)
 
-    vinkel_B = 180-vinkel_A-vinkel_C;
+    vinkel_B = 180-vinkel_A-vinkel_C;//Finder vinkel B
 
     printf("\nVinkel A: %f\nVinkel B: %f\nVinkel C: %d\nSide   b: %f \n",vinkel_A,vinkel_B,vinkel_C, hosliggende_katet);
 
@@ -60,13 +60,13 @@ int main(){
     else
     {
 
-    vinkel_A = acos(hosliggende_katet/hypotenusen);//cosinus
-    vinkel_A = radiantodegree(vinkel_A);//brug af funktion
+    vinkel_A = acos(hosliggende_katet/hypotenusen);//cosinus^-1 
+    vinkel_A = radiantodegree(vinkel_A);//brug af funktion der regner fra radians til grader
 
 
-    modstaende_katete = sqrt((hypotenusen*hypotenusen - hosliggende_katet*hosliggende_katet));//pytagoras læresætning
+    modstaende_katete = sqrt((hypotenusen*hypotenusen - hosliggende_katet*hosliggende_katet));//pytagoras læresætning isoleret til a=sqrt(hypotenusen^2 - hosliggende katete^2)
 
-    vinkel_B = 180-vinkel_A-vinkel_C;
+    vinkel_B = 180-vinkel_A-vinkel_C;//Finder vinkel B
 
     printf("\nVinkel A: %f\nVinkel B: %f\nVinkel C: %d\nSide   a: %f \n",vinkel_A,vinkel_B,vinkel_C, modstaende_katete);
 
@@ -79,7 +79,7 @@ int main(){
 
     }
 
-float radiantodegree(float yote)
+float radiantodegree(float yote)//funktion der regne fra radians to grader
 {
     return yote*(180/ M_PI);
 
